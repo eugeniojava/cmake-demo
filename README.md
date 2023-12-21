1e4ba425fe6360990b797b40a29dd5644f093165
Eugênio Moreira <me@eugeniomoreira.com>
2023-12-20 21:08:45 -0300

    upload source

---
+++ README.md
@@ -0,0 +1,8 @@
+# cmake-demo
+
+```sh
+$ cd build
+$ cmake ../src
+$ cmake --build .
+$ ./Debug/demo
+```
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "demo.h"
+
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Version major: " << VERSION_MAJOR << std::endl;
+  std::cout << "Version minor: " << VERSION_MINOR << std::endl;
+  return 0;
+}
